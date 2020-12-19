ceaf290dcd4b7cc87fa8975f9b6d163479feb905
Henry Salliss <henrysalliss@gmail.com>
2020-12-19 22:09:02 +0000

    bingo card created

---
+++ first_script.rb
@@ -0,0 +1,4 @@
+if (2 + 2) == 4 then puts "this formula is correct"
+else if (2 + 2) == 5 then puts "this formula is incorrect"
+else puts "i have no idea how you got any other answer brother"
+end
---
+++ numerology.rb
@@ -0,0 +1,6 @@
+birthdate = gets
+06072000
+birthdate_number = [0 , 6, 0, 7, 2, 0, 0, 0, 0]
+sum = 0
+birthdate_number.each { |a| sum+=a}
+ 
